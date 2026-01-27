23c6e434bee63f3cfff1bb82991bda014e3d7a84
unknown <wendwosen03@gmail.com>
2026-01-27 10:02:44 +0300

    function based views and class based views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from . import views
-
+from .views import list_books, LibraryDetailView  
 urlpatterns = [
-  path('books/',views.book_list,name='book-list'),
-  path('library/<int:pk>/',views.LibraryDetailView.as_view(),name='library-detail'),
+    path('books/', list_books, name='book-list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
 ]
-
